b04197af425615656fde68db13287c358cdd0e66
Adrian <adrianfakstorp@gmail.com>
2016-11-22 16:07:42 -0800

    updating my-example-config

--- my-example-config.py
+++ my-example-config.py
@@ -1,3 +1,6 @@
 API_KEY = 'https://API.ExampleURL.com//api/example/8F34'
 jsonSource = 'jsonCase Storage'
 caseList = 'List of Cases.json'
+prizeRank = 'price_ranking.json'
+
+#Apart from API_KEY, the configs listed above are the defaults.
